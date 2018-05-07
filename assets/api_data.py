25faec46a7206956aea5e9c75a92b2e248bbf362
Musafir <17235733+msfrpy@users.noreply.github.com>
2018-05-07 17:59:29 +0200

    Add files via upload

---
+++ assets/api_data.py
@@ -0,0 +1,14 @@
+api_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+
+place_types = ['accounting', 'airport', 'amusement_park', 'aquarium', 'art_gallery', 'atm', 'bakery', 'bank', 
+               'bar', 'beauty_salon', 'bicycle_store', 'book_store', 'bowling_alley', 'bus_station', 'cafe', 
+               'campground', 'car_dealer', 'car_rental', 'car_repair', 'car_wash', 'casino', 'cemetery', 'church', 
+               'city_hall', 'clothing_store', 'convenience_store', ' courthouse', ' dentist', ' department_store', 
+               'doctor', ' electrician', ' electronics_store', ' embassy', ' fire_station', ' florist', ' funeral_home', 
+               'furniture_store', 'gas_station', 'gym', 'hair_care', 'hardware_store', 'hindu_temple', 'home_goods_store', 
+               'hospital', 'insurance_agency', 'jewelry_store', 'laundry', 'lawyer', 'library', 'liquor_store', 'local_government_office', 
+               'locksmith', 'lodging', 'meal_delivery', 'meal_takeaway', 'mosque', 'movie_rental', 'movie_theater', 'moving_company', 
+               'museum', 'night_club', 'painter', 'park', 'parking', 'pet_store', 'pharmacy', 'physiotherapist', 'plumber', 'police', 
+               'post_office', 'real_estate_agency', 'restaurant', 'roofing_contractor', 'rv_park', 'school', 'shoe_store', 
+               'shopping_mall', 'spa', 'stadium', 'storage', 'store', 'subway_station', 'supermarket', 'synagogue', 'taxi_stand', 
+               'train_station', 'transit_station', 'travel_agency', 'veterinary_care', 'zoo']
---
+++ assets/art.py
@@ -0,0 +1,10 @@
+ascii_art = """
+  ___ _         _ ___ ___ _                
+ | __(_)_ _  __| |_  ) _ \ |__ _ __ ___ ___
+ | _|| | ' \/ _` |/ /|  _/ / _` / _/ -_|_-<
+ |_| |_|_||_\__,_/___|_| |_\__,_\__\___/__/ 0.1
+ by Musafir (2018)                                          """
+
+welcome_text = '''Script locates 2 given places in certain distance from each other based on Google Maps search. 
+/For exmaple: lists all bus stops with pharmacies 20 metres away in Berlin/'''
+
